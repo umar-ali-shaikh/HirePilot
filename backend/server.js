88d5ca557c9bfacc69a5a3785a28c90aa1de0924
Your Name <you@example.com>
2026-07-06 13:43:22 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-dns.setDefaultResultOrder("ipv4first");
+if (process.env.NODE_ENV !== "production") {
+  const dns = require("dns");
+  dns.setServers(["8.8.8.8", "8.8.4.4"]);
+  dns.setDefaultResultOrder("ipv4first");
+}
 
 require("dotenv").config();
 
-
 const app = require("./src/app");
 const connectToDB = require("./src/config/db");
-const invokeGeminiAi = require("./src/services/ai.service")
 
 connectToDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
